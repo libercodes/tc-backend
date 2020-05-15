c8114816fd5298e3fd3a1f10900b10815aaf1fea
liber <libermenna@gmail.com>
2020-05-15 18:00:00 -0300

    Now ConsultarSesion returns an array of sesiones with the usuario, populated with its username

--- src/case of use/admin/Sesion.ts
+++ src/case of use/admin/Sesion.ts
@@ -1,6 +1,11 @@
 import Sesion, { ISesion } from "../../model/Sesion"
 
 export const ConsultarSesiones = async(): Promise<ISesion[]>=> {
-    let sesiones = await Sesion.find()
+    let sesiones = await 
+        Sesion
+            .find()
+            .populate('usuario', 'nombreDeUsuario')
+            .exec()
+    console.log(sesiones[0])
     return sesiones
 }
